dfb6855a23633b63a4aa8a26eb788a00f18ebb3a
aynefd <aynedelgado@gmail.com>
2015-02-01 13:38:01 -0700

    Create stack.h
    template stack

---
+++ stack.h
@@ -0,0 +1,84 @@
+// FILE: stack1.h (part of the namespace main_savitch_7A)
+// TEMPLATE CLASS PROVIDED: stack<T>
+//
+// CONSTRUCTOR for the stack<T> template class:
+//   stack( )
+//     Postcondition: The stack has been initialized as an empty stack.
+//
+// MODIFICATION MEMBER FUNCTIONS for the stack<T> class:
+//   void push(const T& entry)
+//     Precondition: size( ) < CAPACITY.
+//     Postcondition: A new copy of entry has been pushed onto the stack.
+//
+//   void pop( )
+//     Precondition: size( ) > 0.
+//     Postcondition: The top item of the stack has been removed.
+//
+// CONSTANT MEMBER FUNCTIONS for the stack<T> class:
+//   bool empty( ) const
+//     Postcondition: Return value is true if the stack is empty.
+//
+//   usigned size( ) const
+//     Postcondition: Return value is the total number of items in the stack.
+//
+//   T top( ) const
+//     Precondition: size( ) > 0.
+//     Postcondition: The return value is the top item of the stack (but the
+//     stack is unchanged. This differs slightly from the STL stack (where
+//     the top function returns a reference to the item on top of the stack).
+//
+//-------------------
+
+#ifndef MAIN_SAVITCH_STACK1_H
+#define MAIN_SAVITCH_STACK1_H
+#include <cassert>
+namespace main_savitch_7A
+{
+    template <class T>
+    class stack
+    {
+    public:
+        //Const for Array
+        static const unsigned CAPACITY = 30;
+        // CONSTRUCTOR
+        stack( ) { used = 0; }
+        
+        // MODIFICATION MEMBER FUNCTIONS
+        void push(const T& entry);
+        void pop( );
+        // CONSTANT MEMBER FUNCTIONS
+        bool empty( ) const { return (used == 0); }
+        unsigned size( ) const { return used; }
+        T top( ) const;
+    private:
+        T data[CAPACITY];        // Partially filled array
+        unsigned used;             // How much of array is being used
+    };
+}
+namespace main_savitch_7A
+{
+    template <class T>
+    void stack<T>::push(const T& entry)
+    {
+        assert(size()<CAPACITY);
+        data[used] = entry;
+        used++;
+    }
+
+    template <class T>
+    void stack<T>::pop()
+    {
+        assert(size()>0);
+        used--;
+    
+    }
+
+    template <class T>
+    T stack<T>::top() const
+    {
+        assert(size()>0);
+        return data[used-1];
+    
+    }
+}
+#endif
