9a0a8a80f64d65c182a4430d7adf48d5e045e350
aynefd <aynedelgado@gmail.com>
2015-02-01 13:38:48 -0700

    Create calc.cpp
    int main + functions

---
+++ calc.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <fstream>
+#include "stack.h"
+#include <string>
+using namespace std;
+using namespace main_savitch_7A;
+
+int main()
+{
+    
+    cout<<"testing the stack class..."<<endl;
+    stack<char> myStack;
+   
+   do
+   {
+    
+           
+       
+   }
+    while(!myStack.empty());
+  
+    
+    
+    
+    
+    return 0;
+}
